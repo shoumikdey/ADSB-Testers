d47905f5582e34e6ce8c661066c659d45649950a
Shoumik Dey <shoumikdey123@gmail.com>
2019-05-26 23:03:08 +0530

    Now dumps json to a file

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import json
 import glob
 import sys
+import os
 
-def data(fhandle):
+
+def data(fhandle, filename):
     count = 0
     for line in fhandle:
         line = line.rstrip()
         if line.startswith('@') and line.endswith(';') and (len(line) == 42 or len(line) == 28):
 
             count += 1
-            print(json.dumps({'ADSB in mlat':line, 'Timestamp':line[1:13], 'ADSB message':line[13:len(line)-1]}, indent=4, separators=(',',':')))#encode('utf-8'))
+            data = {'ADSB in mlat':line, 'Timestamp':line[1:13], 'ADSB message':line[13:len(line)-1]}
+            print(json.dumps(data, indent=4, separators=(',',':')))#encode('utf-8'))
+            fullPath = os.path.join("output_json/", filename+".json")
+            fh = open(fullPath, "a")
+            json.dump(data, fh)
+            fh.write('\n')
     if count == 0:
         print("No dump1090 mlat format frame found")
 
@@ -19,10 +26,17 @@ def main():
         files = glob.glob(path)
         for fname in files:
             fhandle = open(fname)
-            data(fhandle)
+            data(fhandle, fname[fname.index('/')+1:])
     else:
-        path = sys.argv[sys.argv.index("--file")+1]
-        data(open(path))
+        if "." not in sys.argv[sys.argv.index("--file")+1]:
+            path = str(sys.argv[sys.argv.index("--file")+1])+"*"
+            files = glob.glob(path)
+            for fname in files:
+                fhandle = open(fname)
+                data(fhandle, fname[fname.index('/')+1:])
+        else:
+            path=sys.argv[sys.argv.index("--file")+1]
+            data(open(path), path[path.index('/')+1:])
 
 
 if __name__ == '__main__':
